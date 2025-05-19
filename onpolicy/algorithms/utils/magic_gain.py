[bugfix] add nan_to_num at softmax to ensure valid values

onpolicy/algorithms/utils/magic_gain.py
@@ -121,7 +121,7 @@ class GraphAttentionGAIN(nn.Module):
         else:
             pass
             
-        # GATv2 attenition mechanism
+        # GATv2 attention mechanism
 
         # [batch_size * N * 1 * num_heads * in_features] --> [batch_size * N * N * num_heads * in_features]
         h_1 = h.unsqueeze(2).repeat(1, 1, self.num_agents, 1, 1)
@@ -141,7 +141,7 @@ class GraphAttentionGAIN(nn.Module):
         adj = adj.unsqueeze(-1)
         # attention (tensor): the matrix of coefficients used for the message aggregation 
         # [batch_size * N * N * num_heads]
-        attention = torch.exp(F.log_softmax(e, dim=2)) 
+        attention = torch.exp(F.log_softmax(e, dim=2)).nan_to_num() 
         # the weights from agents that should not communicate (send messages) will be 0, the gradients from 
         # the communication graph will be preserved in this way
         # add epsilon as per GAIN